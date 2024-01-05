1018ec19fcf042384a44bee6f1c915bdd3f7d386
devansareen01 <devansareen6@gmail.com>
2024-01-05 10:27:50 +0530

    updated route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const userRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
+const postRouter = require('./routes/posts');
 
 dotenv.config();//for congfiguring env file
 
@@ -23,6 +24,7 @@ app.use(morgan("common"));
 app.use('/api/users', userRouter);
 
 app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
 
 app.listen(port, () => {
     console.log(`Backend Server is running on port  ${port}`);
